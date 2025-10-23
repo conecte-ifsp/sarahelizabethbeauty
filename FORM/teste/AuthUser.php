af4b877a3a416a10a23bfc5e686d3030b2bb7282
Mandinhaaa <117762880+Mandinhaaa@users.noreply.github.com>
2025-10-23 00:35:15 -0300

    Add files via upload

---
+++ FORM/teste/AuthUser.php
@@ -0,0 +1,73 @@
+<?php
+
+Class Imagem{
+
+    private $pdo;
+
+    public function __construct($dbname, $host, $user, $senha){
+        try{
+            $this->pdo = new PDO("mysql:dbname=".$dbname.";host=".$host,$user,$senha);
+        }   
+        catch (PDOException $e){
+            echo "Erro com banco de dados: ".$e->getMessage();
+            exit();
+        }
+        catch (Exception $e){
+            echo "Erro generico: ".$e->getMessage();
+            exit();
+        }
+    }
+
+    public function buscarDados(){
+        $res = array();
+        $cmd = $this->pdo->query("SELECT * FROM imagem ORDER BY posicao");
+        $res = $cmd->fetchAll(PDO::FETCH_ASSOC);
+        return $res;
+    }
+
+    public function cadastrarDados($apelido, $tipo, $posicao, $endereco){
+        $cmd = $this->pdo->prepare("SELECT id from imagem WHERE apelido = :a");
+        $cmd->bindValue(":a", $apelido);
+        $cmd->execute();
+        if($cmd->rowCount() > 0){
+            return false;
+        } else {
+            $cmd = $this->pdo->prepare("INSERT INTO imagem (apelido, tipo, posicao, endereco) VALUES (:a, :t, :p, :e)");
+            $cmd->bindValue(":a", $apelido);
+            $cmd->bindValue(":t", $tipo);
+            $cmd->bindValue(":p", $posicao);
+            $cmd->bindValue(":e", $endereco);
+            $cmd->execute();
+            return true;
+        }
+    } 
+
+        public function excluirDados($id){
+            $cmd = $this->pdo->prepare("DELETE FROM imagem WHERE id = :id");
+            $cmd->bindValue(":id",$id);
+            $cmd->execute();
+        }
+
+public function buscarDadosEditar($id){
+    $res = array();
+    $cmd = $this->pdo->prepare("SELECT * FROM imagem WHERE id = :id");
+    $cmd->bindValue(":id", $id);
+    $cmd->execute();
+    $res = $cmd->fetch(PDO::FETCH_ASSOC);
+    return $res;
+}
+
+public function atualizarDados($id, $apelido, $tipo, $posicao, $endereco){
+    
+    $cmd = $this->pdo->prepare("UPDATE imagem SET apelido= :a, tipo = :t, posicao = :p, endereco = :e WHERE id =:id");
+    $cmd->bindValue(":a", $apelido);
+    $cmd->bindValue(":t", $tipo);
+    $cmd->bindValue(":p", $posicao);
+    $cmd->bindValue(":e", $endereco);
+    $cmd->bindValue(":id", $id);
+    $cmd->execute();
+    return true;
+    }
+} 
+
+?>
