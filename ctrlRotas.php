1da3c9e9c49cdc620e99ed0e56a1d76acff0e5d6
vasques <laiss.hoga@aluno.ifsp.edu.br>
2025-11-06 17:57:33 -0300

    Refactor routing logic and improve error handling

--- ctrlRotas.php
+++ ctrlRotas.php
@@ -1,31 +1,38 @@
 <?php
-
 $uri = $_GET["uri"] ?? '';
 
-ob_clean(); 
+ob_clean();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-if (str_starts_with($uri, needle: "public/") || str_starts_with($uri, "app/views/") || preg_match('/\.(css|js|jpg|png|jpeg|gif|svg|json)$/', $uri)) {
+// Rota principal vazia
+if (empty($uri)) {
+    require 'public/index.html'; 
+    exit;
+}
+
+// Rotas para arquivos públicos
+if (str_starts_with($uri, "public/") || str_starts_with($uri, "app/views/") || preg_match('/\.(css|js|jpg|png|jpeg|gif|svg|json)$/', $uri)) {
     $fileExtension = strtolower(pathinfo($uri, PATHINFO_EXTENSION));
     
     if (!file_exists($uri) || is_dir($uri)) {
         http_response_code(404);
-        die("404 - Arquivo não DJAISDJSAID.");
+        die("404 - Arquivo não encontrado.");
     }
 
     if ($fileExtension == 'css') {
         header('Content-Type: text/css');
     }
-
-    if ($fileExtension == 'php' || $fileExtension == 'html' || $fileExtension == 'css' || $fileExtension == 'js' || $fileExtension == 'json') {
+    // Adicione outros headers conforme necessário
+    
+    if (in_array($fileExtension, ['php', 'html', 'css', 'js', 'json'])) {
         require $uri;
     } else {
         ob_clean();
         readfile($uri);
     }
 } else {
+    // Aqui você pode implementar seu roteador para outras URLs
     http_response_code(404);
-    echo "404 - Arquivo não BBBBBBB";
+    echo "404 - Página não Encontrada";
 }
-
