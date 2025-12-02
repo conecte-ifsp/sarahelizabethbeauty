d9c5ec2e52805fcecb1c477687faee40c9335e4a
vasques <laiss.hoga@aluno.ifsp.edu.br>
2025-12-02 14:25:23 -0300

    Refactor database connection to use PDO

--- app/core/conn.php
+++ app/core/conn.php
@@ -1,12 +1,33 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "dbsarah";
+<?php 
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+namespace app\core;
 
-    if ($conn->connect_error) {
-        die("Conexão falhou: " . $conn->connect_error);
+use PDO;
+use PDOException;
+
+class DBConnection extends PDO
+{
+    private $host = "localhost";
+    private $dbname = "dbsarah";
+    private $user = "root";
+    private $pass = "";
+
+    public function __construct()
+    {
+        $dsn = "mysql:host={$this->host};dbname={$this->dbname};charset=utf8";
+
+        try {
+            parent::__construct($dsn, $this->user, $this->pass);
+
+            // Configurações recomendadas
+            $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $this->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+        } catch (PDOException $e) {
+            die("ERRO de conexão: " . $e->getMessage());
+        }
     }
+}
+
 ?>
